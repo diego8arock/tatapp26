1f6e5c5dcfb3e3c973c752cb3402b1c0766411b3
Julio Pinzon <optimus1006@gmail.com>
2018-11-29 10:51:47 -0500

    Modificacion login parte 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,19 @@
 Rails.application.routes.draw do
 
-
-  root 'home#index'
+  root 'sessions#new'
 
   resources :users
-  resources :sessions, only: [:new, :create, :destroy]
   resources :maps
+  resources :sessions, only: [:show, :edit, :new, :create, :destroy]
+  resources :projects do
+    resources :seats
+  end
 
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
 
+  resources :assignments
+  
+  resources :employees
 end
