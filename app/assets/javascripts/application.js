1357c6a9802f04cbd6aee0dd5d042a5598caef21
diego8arock <diegoa2000alt@gmail.com>
2018-12-01 21:15:20 -0500

    Update JS resources.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,25 @@
 //= require jquery3
 //= require jquery_ujs
 //= require_tree .
+
+function textFieldOnlyNumbers(e) {
+  if (!(
+      // Allow: backspace, tab, enter, esc, end, home, left, right, del, point, period
+      $.inArray(e.keyCode, [8, 9, 13, 27, 35, 36, 37, 39, 46, 110, 190]) !== -1 ||
+      // Allow: Ctrl+A, Ctrl+C, Ctrl+X
+      ((e.ctrlKey || e.metaKey) && $.inArray(e.keyCode, [65, 67, 88]) !== -1) ||
+      // Allow: keys 0-9
+      (!e.shiftKey && e.keyCode >= 48 && e.keyCode <= 57) ||
+      // Allow: numpad 0-9
+      (e.keyCode >= 96 && e.keyCode <= 105))) {
+    // Disallow: every other keypress
+    e.preventDefault();
+  }
+}
+
+function randomNumberGenerator() {
+    var minNumber = 0; // The minimum number you want
+    var maxNumber = 100; // The maximum number you want
+    var randomnumber = Math.floor(Math.random() * (maxNumber + 1) + minNumber); // Generates random number
+    return randomnumber;
+}
