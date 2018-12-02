ab86a2faa006878857f7917b4045bd0ffd96a09c
diego8arock <diegoa2000alt@gmail.com>
2018-12-01 21:16:28 -0500

    Add new field for projects.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -69,6 +69,6 @@ class ProjectsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def project_params
-      params.require(:project).permit(:id, :name, :status)
+      params.require(:project).permit(:name, :status, :tag)
     end
 end
