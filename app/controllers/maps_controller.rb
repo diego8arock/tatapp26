f7e7911e1967f8d4347b71f5cd4e5cb75b714faf
diego8arock <diegoa2000alt@gmail.com>
2018-12-01 21:16:12 -0500

    Add saving method of maps with projects.

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,6 +1,7 @@
 class MapsController < ApplicationController
   before_action :set_map, only: [:edit, :update, :destroy]
   before_action :set_maps, only: [:show]
+  before_action :set_maps_by_first_project, only: [:show]
 
   def new
     @map = Map.new
@@ -10,6 +11,16 @@ class MapsController < ApplicationController
   end
 
   def show
+    if params[:project_id].present?
+      @maps = Map.where(project_id: params[:project_id])
+    end
+    if request.xhr?
+      respond_to do |format|
+        format.json {
+          render json: {maps: @maps}
+        }
+      end
+    end
   end
 
   def create
@@ -36,8 +47,12 @@ class MapsController < ApplicationController
       @maps = Map.all
     end
 
+    def set_maps_by_first_project
+      @map_by_first_project = Map.where(project_id: Project.first(1))
+    end
+
     # Never trust parameters from the scary internet, only allow the white list through.
     def map_params
-      params.require(:map).permit(:name, :seats, :height, :width, :seats_total)
+      params.require(:map).permit(:name, :seats, :height, :width, :seats_total, :project_id)
     end
 end
