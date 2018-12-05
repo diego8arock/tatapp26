50a2d1734de843cb9928e0ba2fab6cd7d843aa71
diego8arock <diegoa2000alt@gmail.com>
2018-12-04 20:31:38 -0500

    Restore deleted lines.

--- app/models/seat.rb
+++ app/models/seat.rb
@@ -1,6 +1,8 @@
 class Seat < ApplicationRecord
 
-
+  belongs_to :project, :foreign_key => "id_project"
+  belongs_to :map, :foreign_key => "id_map"
+  has_many :project_employees
 
     ACTIVE = 1
     INACTIVE = 2
